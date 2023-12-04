79eab2d4668d210eccd7fd72ff6f1a85580f3567
Bit Handshake <adam@monotech.hu>
2023-12-05 00:50:11 +0100

    0.0.3.7

--- source-code/cljc/syntax_interpreter/default_patterns.clj
+++ source-code/cljc/syntax_interpreter/default_patterns.clj
@@ -38,12 +38,12 @@
       :symbol     [#"(?<=[\n\r\s\t\[\]\(\)\{\}\"])[a-zA-Z\d\+\-\*\/\=\<\>\!\?\_\%\&\.\~\^][a-zA-Z\d\+\-\*\/\=\<\>\!\?\_\%\&\.\~\^\#\'\:]{0,}(?=[\n\r\s\t\[\]\(\)\{\}\"\@])"     {:pattern-limits {:lookbehind 1          :lookahead 1}}]
       :unresolved [#"(?<=[\n\r\s\t\[\]\(\)\{\}\"])\'[a-zA-Z\d\+\-\*\/\=\<\>\!\?\_\%\&\.\~\^][a-zA-Z\d\+\-\*\/\=\<\>\!\?\_\%\&\.\~\^\#\'\:]{0,}(?=[\n\r\s\t\[\]\(\)\{\}\"\@])"   {:pattern-limits {:lookbehind 1          :lookahead 1}}]
       :var        [#"(?<=[\n\r\s\t\[\]\(\)\{\}\"])\#\'[a-zA-Z\d\+\-\*\/\=\<\>\!\?\_\%\&\.\~\^][a-zA-Z\d\+\-\*\/\=\<\>\!\?\_\%\&\.\~\^\#\'\:]{0,}(?=[\n\r\s\t\[\]\(\)\{\}\"\@])" {:pattern-limits {:lookbehind 1          :lookahead 1}}]
-      :list       [#"\(" #"\)" {:pattern-limits {:lookbehind 0 :match 1 :lookahead 0}}]
-      :map        [#"\{" #"\}" {:pattern-limits {:lookbehind 0 :match 1 :lookahead 0}}]
-      :vector     [#"\[" #"\]" {:pattern-limits {:lookbehind 0 :match 1 :lookahead 0}}]
-      :comment    [#";"  #"\n" {:priority :high :disable-interpreter? true :pattern-limits {:lookbehind 0 :match 1 :lookahead 0}}]
-      :regex      [#"\"" #"\"" {:priority :high :disable-interpreter? true :pattern-limits {:lookbehind 0 :match 1 :lookahead 0}}]
-      :string     [#"\"" #"\"" {:priority :high :disable-interpreter? true :pattern-limits {:lookbehind 0 :match 1 :lookahead 0}}]})
+      :list       [#"\("   #"\)" {:pattern-limits {:lookbehind 0 :match 1 :lookahead 0}}]
+      :map        [#"\{"   #"\}" {:pattern-limits {:lookbehind 0 :match 1 :lookahead 0}}]
+      :vector     [#"\["   #"\]" {:pattern-limits {:lookbehind 0 :match 1 :lookahead 0}}]
+      :comment    [#";"    #"\n" {:priority :high :disable-interpreter? true :pattern-limits {:lookbehind 0 :match 1 :lookahead 0}}]
+      :regex      [#"\#\"" #"\"" {:priority :high :disable-interpreter? true :pattern-limits {:lookbehind 0 :match 2 :lookahead 0}}]
+      :string     [#"\""   #"\"" {:priority :high :disable-interpreter? true :pattern-limits {:lookbehind 0 :match 1 :lookahead 0}}]})
 
 ; @important
 ; Providing lookbehind, lookahead and match length limits helps decrease the processing time.
